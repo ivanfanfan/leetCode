0e52dafd96fa5e3800075aeb544df31e37ede310
wangerfan <ivan_sir@163.com>
2020-08-29 23:18:47 +0800

    递归思想 2020年8月29日23:17:53

--- src/javacore/recursion/Recursion1001.java
+++ src/javacore/recursion/Recursion1001.java
@@ -1,5 +1,15 @@
 package javacore.recursion;
 
 public class Recursion1001 {
+    public static void main(String[] args) {
+        int num = new Recursion1001().getNum(100);
+        System.out.println(num);
+    }
+    public int getNum(int a){
+        if(a==0){
+            return 0;
+        }
+        return a+getNum(a-1);
 
+    }
 }
--- src/leetcode/s24/S2.java
+++ src/leetcode/s24/S2.java
@@ -1,31 +1,35 @@
 package leetcode.s24;
 
 /**
+ * TODO 递归的思想 源码
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
  *     ListNode next;
  *     ListNode(int x) { val = x; }
  * }
+ *
+ * 从链表的头节点 head 开始递归。
+ * 每次递归都负责交换一对节点。由 firstNode 和 secondNode 表示要交换的两个节点。
+ * 下一次递归则是传递的是下一对需要交换的节点。若链表中还有节点，则继续递归。
+ * 交换了两个节点以后，返回 secondNode，因为它是交换后的新头。
+ * 在所有节点交换完成以后，我们返回交换后的头，实际上是原始链表的第二个节点。
+ *
  */
 class S2 {
+    public static void main(String[] args) {
+
+    }
     public ListNode swapPairs(ListNode head) {
 
         // 递归结束的条件
         if ((head == null) || (head.next == null)) {
             return head;
         }
-
-        // Nodes to be swapped
-        ListNode firstNode = head;
-        ListNode secondNode = head.next;
-
-        // Swapping
-        firstNode.next = swapPairs(secondNode.next);
-        secondNode.next = firstNode;
-
-        // Now the head is the second node
-        return secondNode;
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
     }
 }
 
